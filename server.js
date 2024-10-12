7c38e12edc140eec16fe26e87310c78207bb550b
Frank <fbrp.2003@gmail.com>
2024-10-11 21:48:47 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
